c46bb35f6157d906c57a408868399efd084e9b59
Sven Anton <svenanton16@gmail.com>
2023-12-21 13:15:32 +0200

    pilvio webserver tutorial: setting everything up

--- pilvio-webserver/variables.tf
+++ pilvio-webserver/variables.tf
@@ -6,3 +6,12 @@ variable "pilvio" {
   description = "Information about Pilvio account"
   sensitive = true
 }
+
+variable "vm" {
+  type = object({
+    username = string
+    password = string
+  })
+  description = "Sensitive config values for VM"
+  sensitive = true
+}
